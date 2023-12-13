4b70f5917baf55ffc319e05e1a64191b4e20ffdb
n.bodinus <n.bodinus@edu.letteverein.berlin>
2023-12-13 15:25:59 +0100

    Neue Funktionen hinzugefügt

--- PlayerList.py
+++ PlayerList.py
@@ -1,8 +1,9 @@
 import pygame
+from UnitsList import Unit
 
 # Player Class
 class Player:
-    def __init__(self, player_units):
+    def __init__(self):
         #stats
         self.health = 100
         self.level = 1
@@ -10,31 +11,44 @@ class Player:
         self.gold = 0
 
         #All units the player buys and has on the field
-        self.player_units = player_units
+        self.player_units = []
+
         #checking for round-based Gameplay
         self.is_turn = False
 
+    #Earn gold after every Round, set Amount
     def earn_gold(self, amount):
         self.gold += amount
 
+    #Make it so a Player can spend Gold in the Shop
     def spend_gold(self, amount):
         if self.gold >= amount:
             self.gold -= amount
             return True
         return False
     
+    #Make it so that Players have a Way to get experience Points
     def gain_experience(self, amount):
         self.experience += amount
         self.check_level_up()
 
+    #Check if the Player gets a level up after a certain Amount of experience Points
     def check_level_up(self):
         level_up_cost = 5  # Gold cost per experience point
         while self.experience >= self.level * level_up_cost:
             self.level_up()   
-             
+
+    #Level the Player up for 1 level      
     def level_up(self):
         level_up_cost = 5  # Gold cost per experience point
         self.experience -= self.level * level_up_cost
         self.level += 1
         print(f"Leveled up to level {self.level}!")
-        
+    
+    #Units can be added to the Players own Unit List
+    def add_unit(self, Unit):
+        self.player_units.append(Unit)
+
+    #Units can be removed from the Players own Unit List
+    def remove_unit(self, unit):
+        self.player_units.remove(unit)    
--- ShopList.py
+++ ShopList.py
@@ -1,7 +1,26 @@
 import pygame
+from Game import Game
+from PlayerList import Player
+from UnitsList import Unit, Warrior, ADC, Assasine, Mage
 
 # Bekommt die unit listen vom Game
 class Shop:
     def __init__(self, game):
-        self.available_units = []
-        
+        self.unitlist = game.unitlist
+
+        #warrior_unitlist = []
+        #for warrior in self.unitlist:
+            #warrior_unitlist.append(warrior)
+
+        #adc_unitlist = []
+        #for adc in self.unitlist:
+            #adc_unitlist.append(adc)
+
+    def buy_unit(self, unit, player):
+        if player.spend_gold(unit.cost):
+            player.add_unit(unit)
+            self.unitlist.remove(unit)       
+
+    def sell_unit(self, unit, player):
+        player.remove_unit(unit)
+        self.unitlist.append(unit)
